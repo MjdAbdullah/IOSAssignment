bf9dbf7a0c2d7253301a9c5fc2df440b604781ac
MjdAbdullah <mjd.abdullah75@gmail.com>
2021-12-06 21:05:41 +0000

    TTT

--- IOS BASICS/TTT/TTT/ViewController.swift
+++ IOS BASICS/TTT/TTT/ViewController.swift
@@ -11,14 +11,76 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        reset()
     }
     
     var player = "Red"
-    var button = ""
     
     @IBOutlet weak var b1: UIButton!
     @IBAction func b1(_ sender: UIButton) {
+        switchColor(sender: sender)
+    }
+    
+    @IBOutlet weak var b2: UIButton!
+    @IBAction func b2(_ sender: UIButton) {
+        switchColor(sender: sender)
+    }
+    
+    @IBOutlet weak var b3: UIButton!
+    @IBAction func b3(_ sender: UIButton) {
+        switchColor(sender: sender)
+    }
+    
+    @IBOutlet weak var b4: UIButton!
+    @IBAction func b4(_ sender: UIButton) {
+        switchColor(sender: sender)
+    }
+    
+    @IBOutlet weak var b5: UIButton!
+    @IBAction func b5(_ sender: UIButton) {
+        switchColor(sender: sender)
+    }
+    
+    @IBOutlet weak var b6: UIButton!
+    @IBAction func b6(_ sender: UIButton) {
+        switchColor(sender: sender)
+    }
+    
+    @IBOutlet weak var b7: UIButton!
+    @IBAction func b7(_ sender: UIButton) {
+        switchColor(sender: sender)
+    }
+    
+    @IBOutlet weak var b8: UIButton!
+    @IBAction func b8(_ sender: UIButton) {
+        switchColor(sender: sender)
+    }
+    
+    @IBOutlet weak var b9: UIButton!
+    @IBAction func b9(_ sender: UIButton) {
+        switchColor(sender: sender)
+    }
+    
+    
+    @IBAction func bReset(_ sender: Any) {
+        reset()
+    }
+    
+    func checkWon(){
+        
+    }
+    func reset(){
+        b1.backgroundColor = UIColor.lightGray
+        b2.backgroundColor = UIColor.lightGray
+        b3.backgroundColor = UIColor.lightGray
+        b4.backgroundColor = UIColor.lightGray
+        b5.backgroundColor = UIColor.lightGray
+        b6.backgroundColor = UIColor.lightGray
+        b7.backgroundColor = UIColor.lightGray
+        b8.backgroundColor = UIColor.lightGray
+        b9.backgroundColor = UIColor.lightGray
+    }
+    func switchColor(sender : UIButton){
         if sender.backgroundColor == UIColor.lightGray{
             if player == "Red"{
                 sender.backgroundColor = UIColor.red
@@ -30,35 +92,5 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var b2: UIButton!
-    @IBAction func b2(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func b3(_ sender: Any) {
-    }
-    
-    @IBAction func b4(_ sender: Any) {
-    }
-    
-    @IBAction func b5(_ sender: Any) {
-    }
-    
-    @IBAction func b6(_ sender: Any) {
-    }
-    
-    @IBAction func b7(_ sender: Any) {
-    }
-    
-    @IBAction func b8(_ sender: Any) {
-    }
-    
-    @IBAction func b9(_ sender: Any) {
-    }
-    
-    
-    @IBAction func bReset(_ sender: Any) {
-        b1.backgroundColor = UIColor.lightGray
-    }
 }
 
