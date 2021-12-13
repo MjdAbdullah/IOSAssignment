9d137538ed0fc6f3f99f6b323e13789d74d04815
MjdAbdullah <mjd.abdullah75@gmail.com>
2021-12-13 21:25:32 +0000

    BinrayCounter

--- IOS BASICS/BinrayCounter/BinrayCounter/TableViewCell.swift
+++ IOS BASICS/BinrayCounter/BinrayCounter/TableViewCell.swift
@@ -7,18 +7,27 @@
 
 import UIKit
 
+protocol ChangeValue: class{
+    func ChangeTotal(value: Int)
+}
+
 class TableViewCell: UITableViewCell {
+    
+    weak var delegate: ChangeValue?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     @IBAction func bPlus(_ sender: UIButton) {
-        
+        let value = Int(lblNumber.text!)!
+        delegate?.ChangeTotal(value: value)
     }
     
     @IBAction func bMinus(_ sender: UIButton) {
-        
+        var value = Int(lblNumber.text!)!
+        value = -value
+        delegate?.ChangeTotal(value: value)
     }
     
     @IBOutlet weak var lblNumber: UILabel!
--- IOS BASICS/BinrayCounter/BinrayCounter/ViewController.swift
+++ IOS BASICS/BinrayCounter/BinrayCounter/ViewController.swift
@@ -7,12 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,ChangeValue {
     
-
+    func ChangeTotal(value: Int) {
+        total += value
+        lblTotale.text = "Total: \(total)"
+    }
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var lblTotale: UILabel!
     var arrNumber = [Int]()
+    var total = 0
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return arrNumber.count
@@ -21,6 +26,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
         let data = arrNumber[indexPath.row]
+        cell.delegate = self
         cell.setUpNumber(num: data)
         return cell
     }
@@ -30,25 +36,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         
-        arrNumber.append(Int(pow(10.0, 0.0)))
-        arrNumber.append(Int(pow(10.0, 1.0)))
-        arrNumber.append(Int(pow(10.0, 2.0)))
-        arrNumber.append(Int(pow(10.0, 3.0)))
-        arrNumber.append(Int(pow(10.0, 4.0)))
-        arrNumber.append(Int(pow(10.0, 5.0)))
-        arrNumber.append(Int(pow(10.0, 6.0)))
-        arrNumber.append(Int(pow(10.0, 7.0)))
-        arrNumber.append(Int(pow(10.0, 8.0)))
-        arrNumber.append(Int(pow(10.0, 9.0)))
-        arrNumber.append(Int(pow(10.0, 10.0)))
-        arrNumber.append(Int(pow(10.0, 11.0)))
-        arrNumber.append(Int(pow(10.0, 12.0)))
-        arrNumber.append(Int(pow(10.0, 13.0)))
-        arrNumber.append(Int(pow(10.0, 14.0)))
-        arrNumber.append(Int(pow(10.0, 15.0)))
-        arrNumber.append(Int(pow(10.0, 16.0)))
+        for i in 0...16{
+            arrNumber.append(Int(pow(10.0, Double(i))))
+        }
+        
+        lblTotale.text = "Total: 0"
     }
-
-
 }
-
